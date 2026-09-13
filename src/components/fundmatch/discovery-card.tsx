a74a4be4f90cea919bd831e0a06b8b75fbbadb5a
WGLewis0721 <William.GLewis17@gmail.com>
2026-09-12 20:11:30 -0500

    Slow discovery swipe motion by 10 percent

--- src/components/fundmatch/discovery-card.tsx
+++ src/components/fundmatch/discovery-card.tsx
@@ -120,6 +120,7 @@ export function DiscoveryCard({
           "--drag": `${offset}px`,
           "--tilt": `${offset / 28}deg`,
           touchAction: "pan-y",
+          transitionDuration: "0.154s",
         } as CSSProperties
       }
       onKeyDown={(e) => {
